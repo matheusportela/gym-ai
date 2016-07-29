a08fe711d9420d12a84192800d3fb36cda84d049
Matheus Portela <matheus.v.portela@gmail.com>
2016-07-29 20:23:51 -0300

    Try to extract ball location from observation.

--- example.py
+++ example.py
@@ -1,4 +1,48 @@
 import gym
+import numpy as np
+import theano
+import theano.tensor as T
+
+
+class ObjectLocator(object):
+    def __init__(self, color):
+        self.color = color
+
+    def locate(self, image):
+        mask = self.generate_mask(image)
+        morphology = self.extract_morphology(image, mask)
+        top_left, bottom_right = self.find_extremes(morphology)
+        return self.calculate_center(top_left, bottom_right)
+
+    def generate_mask(self, image):
+        dim_size = image.shape[0] * image.shape[1]
+        mask_values = []
+
+        for i in range(len(self.color)):
+            mask_values += [self.color[i]] * dim_size
+
+        mask = np.array(mask_values).reshape(image.shape)
+
+        return mask
+
+    def extract_morphology(self, image, mask):
+        print image == mask
+        return (image == mask).astype(int)[0]
+
+    def find_extremes(self, morphology):
+        indices_x = np.array(range(3)*3).reshape([3, 3])
+        indices_y = indices_x.T
+
+        min_x = np.min(indices_x[morphology != 0])
+        max_x = np.max(indices_x[morphology != 0])
+        min_y = np.min(indices_y[morphology != 0])
+        max_y = np.max(indices_y[morphology != 0])
+
+        return np.array([min_y, min_x]), np.array([max_y, max_x])
+
+    def calculate_center(self, top_left, bottom_right):
+        center = (top_left + bottom_right)/2.0
+        return center.astype(int)
 
 
 def main():
@@ -6,7 +50,11 @@ def main():
     print env.action_space
     print env.observation_space
 
+    ball_locator = ObjectLocator(np.array([236, 236, 236]))
+
     observation = env.reset()
+    print observation.shape
+    print ball_locator.locate(observation)
 
     while True:
         env.render()
