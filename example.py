9919cf28dab98bc201412515fe6e34c84634661c
Matheus Portela <matheus.v.portela@gmail.com>
2016-07-30 19:28:42 -0300

    Finished writing object locator.

--- example.py
+++ example.py
@@ -1,37 +1,57 @@
 import gym
 import numpy as np
-import theano
-import theano.tensor as T
 
 
 class ObjectLocator(object):
-    def __init__(self, color):
+    """Locates an object center of mass using its RGB color."""
+    def __init__(self, color, min_y=34, max_y=193):
+        """Parameters:
+        color -- RGB color to locate, in numpy array format.
+        min_y -- Minimum Y coordinate to consider when locating the object.
+        max_y -- Maximum Y coordinate to consider when locating the object.
+        """
         self.color = color
+        self.min_y = min_y
+        self.max_y = max_y
 
     def locate(self, image):
-        mask = self.generate_mask(image)
-        morphology = self.extract_morphology(image, mask)
-        top_left, bottom_right = self.find_extremes(morphology)
-        return self.calculate_center(top_left, bottom_right)
+        """Locate object's center of mass in RGB image.
 
-    def generate_mask(self, image):
-        dim_size = image.shape[0] * image.shape[1]
-        mask_values = []
+        Parameters:
+        image -- Numpy matrix with RGB channels.
 
-        for i in range(len(self.color)):
-            mask_values += [self.color[i]] * dim_size
+        Return:
+        Numpy array with (Y, X) coordinates.
+        """
+        cropped_image = self._crop(image)
+        morphology = self._extract_morphology(cropped_image)
 
-        mask = np.array(mask_values).reshape(image.shape)
+        try:
+            top_left, bottom_right = self._find_extremes(morphology)
+            return self._calculate_center(top_left, bottom_right)
+        except ValueError:
+            return None
 
-        return mask
+    def _crop(self, image):
+        return image[self.min_y:self.max_y, :, :]
 
-    def extract_morphology(self, image, mask):
-        print image == mask
-        return (image == mask).astype(int)[0]
+    def _extract_morphology(self, image):
+        morphology = None
 
-    def find_extremes(self, morphology):
-        indices_x = np.array(range(3)*3).reshape([3, 3])
-        indices_y = indices_x.T
+        for i, color in enumerate(self.color):
+            channel_image = image[:, :, i]
+            mask = channel_image == color
+
+            if morphology is None:
+                morphology = mask
+            else:
+                morphology *= mask
+
+        return morphology
+
+    def _find_extremes(self, morphology):
+        indices_x = np.array(range(morphology.shape[1])*morphology.shape[0]).reshape(morphology.shape)
+        indices_y = np.array([[i]*morphology.shape[1] for i in range(morphology.shape[0])])
 
         min_x = np.min(indices_x[morphology != 0])
         max_x = np.max(indices_x[morphology != 0])
@@ -40,25 +60,37 @@ class ObjectLocator(object):
 
         return np.array([min_y, min_x]), np.array([max_y, max_x])
 
-    def calculate_center(self, top_left, bottom_right):
+    def _calculate_center(self, top_left, bottom_right):
         center = (top_left + bottom_right)/2.0
         return center.astype(int)
 
 
+def print_locations(right_player_location, left_player_location,
+                    ball_location):
+    print 'Right player:', right_player_location
+    print 'Left player:', left_player_location
+    print 'Ball:', ball_location
+    print
+
+
 def main():
     env = gym.make('Pong-v0')
-    print env.action_space
-    print env.observation_space
 
+    right_player_locator = ObjectLocator(np.array([92, 186, 92]))
+    left_player_locator = ObjectLocator(np.array([213, 130, 74]))
     ball_locator = ObjectLocator(np.array([236, 236, 236]))
 
     observation = env.reset()
-    print observation.shape
-    print ball_locator.locate(observation)
+    print_locations(right_player_locator.locate(observation),
+                    left_player_locator.locate(observation),
+                    ball_locator.locate(observation))
 
     while True:
         env.render()
         observation, reward, done, info = env.step(env.action_space.sample())
+        print_locations(right_player_locator.locate(observation),
+                        left_player_locator.locate(observation),
+                        ball_locator.locate(observation))
 
         if done:
             print 'Finished'
