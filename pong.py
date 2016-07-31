851635f2aee8a0a7bbcb1982411e5e8975acbda8
Matheus Portela <matheus.v.portela@gmail.com>
2016-07-30 21:25:21 -0300

    Implementing Q-learning agent.

--- pong.py
+++ pong.py
@@ -65,6 +65,237 @@ class ObjectLocator(object):
         return center.astype(int)
 
 
+class LearningAlgorithm(object):
+    def learn(self, state, action, reward):
+        """Learn from experience.
+
+        Learn by applying the reward received when transitioning from the
+        current state to the new one by executing an action.
+
+        Parameters:
+        state -- Agent state after executing the action.
+        action -- Executed action.
+        reward -- Reward received after executing the action.
+        """
+        raise (NotImplementedError,
+            '%s does not implement "learn" method' % str(type(self)))
+
+    def act(self, state):
+        """Select an action for the given state.
+
+        By exploiting learned model, the algorithm selects the best action to be
+        executed by the agent.
+
+        Parameters:
+        state -- Agent state to select an action.
+        """
+        raise (NotImplementedError,
+            '%s does not implement "act" method' % str(type(self)))
+
+
+class QLearning(LearningAlgorithm):
+    """Q-learning algorithm implementation.
+
+    Q-learning is a model free reinforcement learning algorithm that tries and
+    learning state values and chooses actions that maximize the expected
+    discounted reward for the current state.
+
+    Instance variables:
+    previous_state -- State in which the algorithm currently is.
+    q_values -- Storage for (state, action) pair estimated values.
+    learning_rate -- Value in [0, 1] interval that determines how much of the
+        new information overrides the previous value. Deterministic scenarios
+        may have optimal results with learning rate of 1, which means the new
+        information completely replaces the old one.
+    discount_factor -- Value in [0, 1) interval that determines the importance
+        of future rewards. 0 makes the agent myopic and greedy, trying to
+        achieve higher rewards in the next step. Closer to 1 makes the agent
+        maximize long-term rewards. Although values of 1 and higher are possible,
+        it may make the expected discounted reward infinite or divergent.
+    """
+
+    def __init__(self, initial_state=0, learning_rate=1, discount_factor=1,
+        actions=None):
+        """Constructor.
+
+        Parameters:
+        initial_state -- State where the algorithm begins.
+        num_states -- Number of states to be represented.
+        num_actions -- Number of actions to be represented.
+        """
+        super(QLearning, self).__init__()
+        self.previous_state = initial_state
+        self.q_values = {}
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+
+        if actions:
+            self.actions = actions
+        else:
+            self.actions = []
+
+    def __str__(self):
+        """Generates Q-values string representation."""
+        results = []
+        results.append('Q-values\n')
+        for state in self.q_values:
+            results.append(str(state))
+            for action in self.q_values[state]:
+                results.append(str(self.q_values[state][action]))
+                results.append('\t')
+            results.append('\n')
+        return ''.join(results)
+
+    def update_state(self, state):
+        """Update Q Learning current state.
+
+        Parameters:
+        state -- State to which the learning algorithm is going.
+        """
+        self.previous_state = state
+
+    def initialize_unknown_state(self, state):
+        """Initialize Q-values for states that were not previously seen.
+
+        Parameters:
+        state -- Environment state.
+        """
+        if state not in self.q_values:
+            self.q_values[state] = {}
+            for action_ in self.actions:
+                self.q_values[state][action_] = 0.0
+
+    def get_q_value(self, state, action):
+        """Get the current estimated value for the state-action pair.
+
+        Parameters:
+        state -- Environment state.
+        action -- Agent action.
+        """
+        self.initialize_unknown_state(state)
+        return self.q_values[state][action]
+
+    def set_q_value(self, state, action, value):
+        """Set a new estimated value for the state-action pair.
+
+        Parameters:
+        state -- Environment state.
+        action -- Agent action.
+        value -- New estimated value.
+        """
+        self.q_values[state][action] = value
+
+    def _get_max_action_from_list(self, state, action_list):
+        """Get the action with maximum estimated value from the given list of
+        actions.
+
+        state -- Environment state.
+        action_list -- Actions to be evaluated.
+        """
+        self.initialize_unknown_state(state)
+        actions = filter(lambda a: a in action_list, self.q_values[state])
+        values = [self.q_values[state][action] for action in actions]
+        max_value = max(values)
+        max_actions = [action
+                       for action in actions
+                       if self.q_values[state][action] == max_value]
+
+        return np.random.choice(max_actions)
+
+    def get_max_action(self, state):
+        """Get the action with maximum estimated value.
+
+        Parameters:
+        state -- Environment state.
+        """
+        self.initialize_unknown_state(state)
+        return self._get_max_action_from_list(state, self.actions)
+
+    def get_max_q_value(self, state):
+        max_action = self.get_max_action(state)
+        return self.q_values[state][max_action]
+
+    def learn(self, state, action, reward):
+        """Learn by updating the (state, action) reward.
+
+        Learn by applying the reward received when transitioning from the
+        current state to the new one by executing an action.
+
+        Parameters:
+        state -- Agent state after executing the action.
+        action -- Executed action.
+        reward -- Reward received after executing the action.
+        """
+        old_value = self.get_q_value(self.previous_state, action)
+        next_expected_value = self.get_max_q_value(state)
+        new_value = (old_value + self.learning_rate*(reward + self.discount_factor*next_expected_value - old_value))
+        self.set_q_value(self.previous_state, action, new_value)
+        self.update_state(state)
+
+    def act(self, state, legal_actions=None):
+        """Select the best legal action for the given state.
+
+        Parameters:
+        state -- Agent state to select an action.
+        legal_actions -- Actions allowed in the current state.
+
+        Return:
+        Action number.
+        """
+        if legal_actions:
+            return self._get_max_action_from_list(state, legal_actions)
+        return self._get_max_action_from_list(state, self.actions)
+
+
+class StateEstimator(object):
+    def __init__(self):
+        self.right_player_locator = ObjectLocator(np.array([92, 186, 92]))
+        self.left_player_locator = ObjectLocator(np.array([213, 130, 74]))
+        self.ball_locator = ObjectLocator(np.array([236, 236, 236]))
+        self.previous_ball_location = np.zeros(2)
+
+    def estimate(self, observation):
+        right_player_location = self.right_player_locator.locate(observation)
+        left_player_location = self.left_player_locator.locate(observation)
+        ball_location = self.ball_locator.locate(observation)
+
+        if ball_location is not None:
+            ball_velocity = ball_location - self.previous_ball_location
+            self.previous_ball_location = ball_location
+        else:
+            ball_velocity = None
+            self.previous_ball_location = np.zeros(2)
+
+        # Convert to string because it's hashable
+        state = str([
+            right_player_location,
+            left_player_location,
+            ball_location,
+            ball_velocity,
+        ])
+
+        return state
+
+
+class Agent(object):
+    def __init__(self, num_actions, exploration_rate=0.05):
+        self.actions = range(num_actions)
+        self.q_learner = QLearning(
+            actions=self.actions,
+            learning_rate=0.9,
+            discount_factor=0.1,
+        )
+        self.exploration_rate = exploration_rate
+
+    def learn(self, state, action, reward):
+        self.q_learner.learn(state, action, reward)
+
+    def act(self, state):
+        if np.random.random() < self.exploration_rate:
+            return np.random.choice(self.actions)
+        return self.q_learner.act(state)
+
+
 def print_locations(right_player_location, left_player_location,
                     ball_location):
     print 'Right player:', right_player_location
@@ -75,27 +306,37 @@ def print_locations(right_player_location, left_player_location,
 
 def main():
     env = gym.make('Pong-v0')
+    print env.action_space
 
-    right_player_locator = ObjectLocator(np.array([92, 186, 92]))
-    left_player_locator = ObjectLocator(np.array([213, 130, 74]))
-    ball_locator = ObjectLocator(np.array([236, 236, 236]))
+    agent = Agent(env.action_space.n)
+    state_estimator = StateEstimator()
 
     observation = env.reset()
-    print_locations(right_player_locator.locate(observation),
-                    left_player_locator.locate(observation),
-                    ball_locator.locate(observation))
+    state = state_estimator.estimate(observation)
+
+    game_number = 1
+    print 'Game #{}'.format(game_number)
+
+    episode_reward = 0
 
     while True:
         env.render()
-        observation, reward, done, info = env.step(env.action_space.sample())
-        print_locations(right_player_locator.locate(observation),
-                        left_player_locator.locate(observation),
-                        ball_locator.locate(observation))
-        print reward
+
+        action = agent.act(state)
+        observation, reward, done, info = env.step(action)
+        state = state_estimator.estimate(observation)
+        agent.learn(state, action, reward)
+
+        episode_reward += reward
 
         if done:
-            print 'Finished'
-            break
+            print 'Episode reward:', episode_reward
+            episode_reward = 0
+
+            game_number += 1
+            print 'Game #{}'.format(game_number)
+
+            observation = env.reset()
 
 if __name__ == '__main__':
     try:
